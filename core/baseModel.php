3ecd6b94a56cc90e93748ecd4b6b9ff87bf98e2b
Jorge <jorgamow@gmail.com>
2020-10-19 15:33:22 -0500

    se añade el baseModel

---
+++ core/baseModel.php
@@ -0,0 +1,37 @@
+<?php
+
+class baseModel extends baseEntity{
+
+	private $table;
+
+	public function __construct($table){
+		$this->table=(string)$table;
+		parent::__construct($table);
+	}
+
+	public function ejecutarSql($query){
+		$query = $this->db()->query($query);
+
+		if($query){
+			if($query->num_rows >1){
+				while ($row=$query->fetch_object()){
+					$resultSet[]=$row;
+				}
+			}elseif   ($query->num_rows==1){
+				if($row=$query->fetch_object()){
+					$resultSet= $row;
+				}
+			}else{
+				$resultSet=true;
+			}
+		}else{
+			$resultSet=false;
+
+		}
+		return $resultSet;	
+	} 
+
+
+
+}
+
